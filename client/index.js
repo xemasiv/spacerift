1149743e051f0e2283d7b22311301efa702a3538
xemasiv <xemasiv@gmail.com>
2018-05-03 04:03:24 +0800

    signals

--- client/index.js
+++ client/index.js
@@ -1,15 +1,44 @@
-const Peer = require('simple-peer');
-const DEBUG = require('debug');
-const debug = DEBUG('Spacerift:debug');
-const info = DEBUG('Spacerift:info');
-info.enabled = true;
-debug.enabled = ENV.debug;
+const DEBUG          = require('debug');
+const axios          = require('axios');
+const Peer           = require('simple-peer');
+const uuid           = require('uuid-random');
+const EventEmitter3  = require('eventemitter3');
+
+const debug     = DEBUG('Spacerift:debug');
+const info      = DEBUG('Spacerift:info');
+info.enabled    = true;
+debug.enabled   = ENV.debug;
+
 class Client{
   constructor (o) {
-    const { host } = o || {};
-    this.host = Boolean(host) ? host : window.location.href;
-    this.peer = new Peer({ initiator: true });
+    this.host = (
+      typeof o === 'object' && Boolean(o.host) === true
+    ) ? o.host : window.location.href;
+    this.peers = [];
+    debug('host:', host);
+    let EventEmitter = new EventEmitter3();
+    this.on = EventEmitter.on.bind(EventEmitter);
+    this.once = EventEmitter.once.bind(EventEmitter);
+    this.emit = EventEmitter.emit.bind(EventEmitter);
+  }
+  discover () {
+    let client = this;
+    let peer = new Peer({ initiator: true });
+    let signalContext = uuid();
+    client.once(signalContext, (signal) => {
+      debug('signal received:');
+      debug(JSON.stringify(signal));
+    });
+    debug('awaiting signal..');
+    peer.on('signal', (signal) => {
+      client.emit(signalContext, signal)
+      client.emit('signal', signal);
+    });
+    client.peers.push(peer);
   }
 }
 window.SpaceriftClient = Client;
+if (ENV.debug) {
+  window.x = new Client();
+}
 info('OK!');
