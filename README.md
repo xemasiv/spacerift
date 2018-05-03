fd4412df4c5a185d20c4a741aecc62dfb0e9590f
xemasiv <xemasiv@gmail.com>
2018-05-03 23:16:58 +0800

    + Immutable state, README update.

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # spacerift
 HTTP-based peer-links with efficient roundtrips.
 
+## Server Basics
+* The server is an `express` instance where each incoming request is fingerprinted to identify clients by device.
+  * `sha3_256` hash of the following:
+    * `useragent`
+    * `ip`
+    * `headers.host`, `headers.accept`, `headers.language`
+* Clients are then Mapped to `clients` object
+  * `clients` is an instance of Map(), where:
+    * `key` : `sha3_256` fingerprint
+    * `value` : Object
+      * `request` : `req` Object
+      * `connection` : `online` String
+* An `onFinished` is also attached to the `req` Object to detect whenever the client disconnects.
+  * This changes the client's `connection` state to `offline`, and this removes the `request` reference to the `req` Object.
+  * Now everytime this client connects, its `connection` state is switched back to `online`, and its `request` value is updated with the new `req` Object.
+
+## Server & Client Plugins
+* With a `plug-and-play` approach in mind, server and clients must work with individual modules instead that can be mixed and matched with each other.
+* This will allow users to just pick any existing modules for their project, or easily craft one for custom purposes.
+
 ## License
 
 Attribution 4.0 International (CC BY 4.0)
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const DEBUG = require('debug');
 const onFinished = require('on-finished');
 const circular = require('circular-json');
 const sha3_256 = require('js-sha3').sha3_256;
+const Immutable = require('immutable');
 
 const debug = DEBUG('Spacerift');
 debug.enabled = true;
@@ -37,30 +38,28 @@ const useragent = require('useragent');
 const geoip = require('geoip-lite');
 const ACTIONS = require('./shared/Enums.js');
 
-const fresh = new Map();
-const awaiting = new Map();
-
+const clients = new Map();
+/*
 const check = () => {
-  debug('running check');
-  debug('fresh.size', fresh.size);
-  debug('got equal pairs', fresh.size % 2 === 0);
+  debug('CHECK', clients.size, clients.size % 2 === 0);
   if (
-    fresh.size > 0 && // if we have any freshies
-    fresh.size % 2 === 0 // and we got pairs for each
+    clients.size > 0 && // if we have any clients
+    clients.size % 2 === 0 // and we got pairs for each
   ) {
     let pair = [];
-    debug('pair lookup');
-    fresh.forEach((value, key) => {
-      debug('across', key);
+    debug('LOOKUP');
+    clients.forEach((value, key) => {
+      debug('ACROSS', key);
       pair.push({key, value});
-      fresh.delete(key);
-      debug('pair now', pair.length);
+      clients.delete(key);
+      debug('@', pair.length);
       if (pair.length === 2) {
-        debug('pair matched!');
+        debug('MATCH');
       }
     });
   };
 };
+*/
 app.use(express.static('dist'))
 app.post('/', (req, res) => {
   const agent = useragent.lookup(req.headers['user-agent']).toJSON();
@@ -70,27 +69,35 @@ app.post('/', (req, res) => {
     accept: req.headers['accept'],
     language: req.headers['accept-language']
   };
-  let signature = sha3_256(circular.stringify({ agent, geo, headers }));
-  debug('START', fresh.size, awaiting.size, signature);
-  debug(req.body);
-  switch (req.body.type) {
-    case ACTIONS.CONNECT:
-      if (awaiting.has(signature) === false) {
-        if (fresh.has(signature) === false) {
-          fresh.set(signature, req);
-          check();
-        }
-      }
-      break;
-    default:
+  let fingerprint = sha3_256(
+    circular.stringify({ agent, geo, headers })
+  );
 
-      break;
-  }
+  let clientState;
+  if (clients.has(fingerprint) === true) {
+    clientState = clients.get(fingerprint);
+    clientState = clientState.set('connection', 'online');
+    clients.set(fingerprint, clientState);
+  } else {
+    clientState = Immutable.Map({
+      fingerprint: fingerprint,
+      request: req,
+      connection: 'online'
+    });
+    clients.set(fingerprint, clientState);
+  };
+  debug('ONLINE', fingerprint);
+  debug('clientState', clientState.toString());
+  debug('CLIENTS SIZE', clients.size);
+
+  debug(req.body);
   onFinished(req, (...args) => {
-    if (fresh.has(signature) === true) {
-      fresh.delete(signature);
-    }
-    debug('END', fresh.size, awaiting.size, signature);
+    let clientState = clients.get(fingerprint);
+    clientState = clientState.set('connection', 'offline');
+    clients.set(fingerprint, clientState);
+    debug('OFFLINE', fingerprint);
+    debug('clientState', clientState.toString());
+    debug('CLIENTS SIZE', clients.size);
   });
 });
 
