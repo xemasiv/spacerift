8af1431736c4d9242f21f0ce638573fd06882e47
xemasiv <xemasiv@gmail.com>
2018-05-04 09:36:30 +0800

    Add helper libs for test

--- test/server.js
+++ test/server.js
@@ -23,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', express.static('./dist'));
+app.use('/static', express.static('./test/static'));
 
 const Spacerift = require('../index.js')({
   debug: true
